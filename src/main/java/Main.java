0543d42b2deeddcc08e3bff3d242c9d323fb5ba9
Arun Kizhakumbadan <arun.kizhakumbad@gmail.com>
2025-10-16 20:53:41 +0200

    2nd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -7,8 +8,6 @@ public class Main {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-
      try {
        ServerSocket serverSocket = new ServerSocket(4221);
 
@@ -16,8 +15,14 @@ public class Main {
        // ensures that we don't run into 'Address already in use' errors
        serverSocket.setReuseAddress(true);
 
-       serverSocket.accept(); // Wait for connection from client.
+       Socket connection = serverSocket.accept(); // Wait for connection from client.
        System.out.println("accepted new connection");
+
+       OutputStream out = connection.getOutputStream();
+       String response = "HTTP/1.1 200 OK\r\n\r\n";
+       out.write(response.getBytes());
+       out.flush();
+
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
