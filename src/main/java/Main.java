81491de1b8278ff5306156595141548a1b357d23
Arun Kizhakumbadan <arun.kizhakumbad@gmail.com>
2025-10-16 22:58:29 +0200

    3rd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,59 @@
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
-
      try {
        ServerSocket serverSocket = new ServerSocket(4221);
+       int connectionLife = 2; //Two requests only for this test.
+
+        System.out.println(" \u2705 Accepting connection..");
 
        // Since the tester restarts your program quite often, setting SO_REUSEADDR
        // ensures that we don't run into 'Address already in use' errors
        serverSocket.setReuseAddress(true);
 
-       Socket connection = serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
-
-       OutputStream out = connection.getOutputStream();
-       String response = "HTTP/1.1 200 OK\r\n\r\n";
-       out.write(response.getBytes());
-       out.flush();
+       while (connectionLife > 0) {
+           try (Socket connection = serverSocket.accept()) {
+               String response = getResponseFromInput(connection);
+               writeResponse(connection, response);
+               connectionLife--;
+           }
+       }
+       System.out.println("\uD83D\uDD12 Closing connection..");
 
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
+  }
+
+  private static String getResponseFromInput(Socket connection) throws IOException {
+      String response = "HTTP/1.1 404 Not Found\r\n\r\n";
+      String path = "";
+      BufferedReader br = new BufferedReader(
+              new InputStreamReader(connection.getInputStream())
+      );
+
+      String firstLine = br.readLine();
+
+      if (firstLine != null) {
+          path = firstLine.split(" ")[1];
+      }
+
+      if(path.equalsIgnoreCase("/")) {
+          response = "HTTP/1.1 200 OK\r\n\r\n";
+      }
+
+      return response;
+  }
+
+  private static void writeResponse(Socket connection, String response) throws IOException {
+      OutputStream out = connection.getOutputStream();
+      System.out.println(" \u2705 Accepted new connection..");
+
+      System.out.println("Writing HTTP response to client..");
+      out.write(response.getBytes());
+      out.flush();
   }
 }
